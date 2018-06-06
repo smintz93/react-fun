c00c154988333a06f5becde0ab1b81d4f328cc1d
Sam <sammintz1993@gmail.com>
2018-06-06 14:36:54 -0500

    set up components folder

--- fundamentals/src/App.js
+++ fundamentals/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects'
 import './App.css';
 
 class App extends Component {
+
+
+
   render() {
     return (
       <div className="App">
- 
+          My app
+          <Projects />
       </div>
     );
   }
